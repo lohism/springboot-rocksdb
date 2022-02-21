9294b3c5484c3096e1ad7b90455b2e3f42a1df6a
lohism <abhimv311@gmail.com>
2022-02-21 19:12:25 +0000

    application done

---
+++ src/main/java/com/lohis/springbootrocksdb/SpringbootRocksdbApplication.java
@@ -0,0 +1,13 @@
+package com.lohis.springbootrocksdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRocksdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRocksdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lohis/springbootrocksdb/SpringbootRocksdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lohis.springbootrocksdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRocksdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
